6307f6b63adbe14e46e02f88c0be40205c913858
Nick Sutterer <apotonick@gmail.com>
2011-04-04 15:28:55 +0200

    TestXml::MatcherMethods now contains the actual tests.

---
+++ lib/test_xml/matcher_methods.rb
@@ -0,0 +1,29 @@
+module TestXml
+  # This module implements the actual matchers with their conditions.
+  module MatcherMethods
+    def self.xml_contain(subject, pattern)
+      actual, expected = parse_xml(subject, pattern)
+      actual.match?(expected, true)
+    end
+    
+    def self.xml_equal(subject, pattern)
+      actual, expected = parse_xml(subject, pattern)
+      actual.match?(expected, true) && expected.match?(actual, true)
+    end
+    
+    def self.xml_structure_contain(subject, pattern)
+      actual, expected = parse_xml(subject, pattern)
+      actual.match?(expected)
+    end
+    
+    def self.xml_structure_equal(subject, pattern)
+      actual, expected = parse_xml(subject, pattern)
+      actual.match?(expected) && expected.match?(actual)
+    end
+    
+  private
+    def self.parse_xml(subject, pattern)
+      [Nokogiri::XML.parse(subject), Nokogiri::XML.parse(pattern)]
+    end
+  end
+end
