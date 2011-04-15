d5b1f4a5f541967b957ea13ebe0412054d5e13cb
Pavel Gabriel <alovak@gmail.com>
2011-04-15 18:04:13 +0300

    message tests for assert_xml and asser_not_ were added

--- test/test_unit/test_assertions.rb
+++ test/test_unit/test_assertions.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class TestAssertions < Test::Unit::TestCase
+  def test_assert_xml_contain_message_if_fails
+    begin 
+      assert_xml_contain("<root><one>1</one><one>2</one></root>", "<root><one>3</one></root>")
+    rescue Exception => e
+      assert_match %r{the xml:\n<root><one>1</one><one>2</one></root>\nshould contain xml:\n<root><one>3</one></root>}, e.message
+    end
+  end
+
+  def test_assert_xml_not_contain_message_if_fails
+    begin 
+      assert_not_xml_contain("<root><one>1</one><one>2</one></root>", "<root><one>1</one><one>2</one></root>")
+    rescue Exception => e
+      assert_match %r{the xml:\n<root><one>1</one><one>2</one></root>\nshould not contain xml:\n<root><one>1</one><one>2</one></root> but it does}, 
+                   e.message
+    end
+  end
+    
   def test_assert_xml_contain
     expected = <<-XML
       <root>
