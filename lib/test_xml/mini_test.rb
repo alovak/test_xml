5d60588a39e03f4ff5ae66f5be8e08b337e53873
Nick Sutterer <apotonick@gmail.com>
2013-04-21 21:30:22 +1000

    make matchers available in MiniTest::Spec.

--- lib/test_xml/mini_test.rb
+++ lib/test_xml/mini_test.rb
@@ -3,4 +3,13 @@ require 'test_xml/test_unit/assertions'
 
 class MiniTest::Unit::TestCase
   include TestXml::TestUnit::Assertions
+  MiniTest::Expectations.class_eval do
+    { :assert_xml_contain           => :must_contain_xml,
+      :assert_xml_structure_contain => :must_contain_xml_structure,
+      :assert_xml_equal             => :must_equal_xml,
+      :assert_xml_equal_structure   => :must_equal_xml_structure
+    }.each_pair do |assertion, matcher|
+      infect_an_assertion assertion, matcher
+    end
+  end
 end
