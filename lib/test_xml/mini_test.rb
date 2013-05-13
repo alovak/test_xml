f45cb30e5ac8a6831dea4d0665b73922763b9d8a
Philip Arndt <parndt@gmail.com>
2013-05-14 03:53:14 +1200

    Support MiniTest 5.0 and above by including assertions inside MiniTest::Test
    Fixes #8

--- lib/test_xml/mini_test.rb
+++ lib/test_xml/mini_test.rb
@@ -4,3 +4,7 @@ require 'test_xml/test_unit/assertions'
 class MiniTest::Unit::TestCase
   include TestXml::TestUnit::Assertions
 end
+
+class MiniTest::Test
+  include TestXml::TestUnit::Assertions
+end
